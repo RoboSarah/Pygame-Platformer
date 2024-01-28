af3a9fd282ad28aeee6152e41ceb1ed5e84ddee7
Basti-usgrf <83810389+Basti-usgrf@users.noreply.github.com>
2024-01-28 17:21:44 +0100

    fix vertical collision

--- level.py
+++ level.py
@@ -10,12 +10,12 @@ class Level:
         self.initalise_level(level_data)
         self.world_shift = 0
 
-# Funktion die durch level list iteriert und tiles plaziert
+    # Funktion die durch level list iteriert und tiles plaziert
     def initalise_level(self, layout):
         self.tiles = pygame.sprite.Group()
         self.player = pygame.sprite.GroupSingle()
 
-# zählt durch reihen und gibt position(index) der reihen aus
+    # zählt durch reihen und gibt position(index) der reihen aus
         for row_index,row in enumerate(layout):
             for column_index,cell in enumerate(row):
             # Multiplizieren da y sonst zu klein und tiles überlagern
@@ -28,7 +28,7 @@ class Level:
                     player_sprite = Player((x,y))
                     self.player.add(player_sprite)
 
-# Bewegung der Kamera in ABhängigkeit vom Spieler
+    # Bewegung der Kamera in ABhängigkeit vom Spieler
     def scroll_x(self):
         player = self.player.sprite
         player_x = player.rect.centerx
@@ -46,13 +46,13 @@ class Level:
             player.speed = 8
 
 
-# Kollisionserkenneung aller Tiles beim vertikal laufen
+    # Kollisionserkenneung aller Tiles beim vertikal laufen
     def horizontal_movement_collission(self):
         player = self.player.sprite
 
         player.rect.x += player.direction.x * player.speed
 
-        for sprite in self.tile.sprites():
+        for sprite in self.tiles.sprites():
             if sprite.rect.colliderect(player.rect):
                 # läuft links
                 if player.direction.x < 0:
@@ -62,39 +62,35 @@ class Level:
                     player.rect.right = sprite.rect.left
 
 
-            
-def vertical_movement_collision(self):
-    player =   self.player.sprite
-    player.apply_gravity() 
+    # TO DO  
+    def vertical_movement_collision(self):
+        player =  self.player.sprite
+        player.apply_gravity() 
 
-    for sprite in self.tile.sprites():
-        if sprite.rect.colliderect(player.rect):
-            # steht auf tile
-            if player.direction.y < 0:
-                player.rect.bottom = sprite.rect.top
-            # y = 0 damit gravitation beim stehen nicht weiterwächst
-                player.direction.y = 0
-            # unter tile
-            elif player.direction.y > 0:
-                player.rect.top = sprite.rect.bottom
-            # aufhebung -y movement
-                player.direction.y = 0
+        for sprite in self.tiles.sprites():
+            if sprite.rect.colliderect(player.rect):
+                # steht auf tile
+                if player.direction.y < 0:
+                    player.rect.bottom = sprite.rect.top
+                # y = 0 damit gravitation beim stehen nicht weiterwächst
+                    player.direction.y = 0
+                # unter tile
+                elif player.direction.y > 0:
+                    player.rect.bottom = sprite.rect.top
+                # aufhebung -y movement
+                    player.direction.y = 0
                 
                 
-
-
-
-
-# Darstellen der verschiedenen Komponenten
+    # Darstellen der verschiedenen Komponenten
     def run(self):
 
-    # tiles
-        self.tiles.update(0)
-        self.tiles.draw(self.display_suface)
+        # tiles
+            self.tiles.update(0)
+            self.tiles.draw(self.display_suface)
 
-    # player
-        self.player.update()
-        self.player.draw(self.display_suface)
-        self.scroll_x()
-        self.horizontal_movement_collission()
-        self.vertical_movement_collision()
+        # player
+            self.player.update()
+            self.player.draw(self.display_suface)
+            self.scroll_x()
+            self.horizontal_movement_collission()
+            self.vertical_movement_collision()
--- player.py
+++ player.py
@@ -18,10 +18,10 @@ class Player(pygame.sprite.Sprite):
         keys= pygame.key.get_pressed()
 
         if keys[pygame.K_RIGHT]:
-            self.direction.x = 1
+            self.direction.x = -1
         
         elif keys[pygame.K_LEFT]:
-           self.direction.x = -1
+           self.direction.x = 1
 
         else:
            self.direction.x = 0
